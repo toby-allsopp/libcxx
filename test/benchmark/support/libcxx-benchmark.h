50423efe62a983963b89051b619b54742664e867
Eric Fiselier <eric@efcs.ca>
2015-01-16 15:13:48 -0500

    stuff

--- test/benchmark/support/libcxx-benchmark.h
+++ test/benchmark/support/libcxx-benchmark.h
@@ -1,7 +1,7 @@
 #ifndef LIBCXX_BENCHMARK_H
 #define LIBCXX_BENCHMARK_H
 
-#include "benchmark/benchmark_minimal.h"
+#include "benchmark/minimal_benchmark.h"
 
 #define BENCHMARK_MAIN()               \
 int main(int argc, const char** argv)  \
