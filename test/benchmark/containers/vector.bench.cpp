976891dafd58b3942c13bea1618a227100a046b0
Eric Fiselier <eric@efcs.ca>
2015-03-04 04:37:22 -0500

    vec test

--- test/benchmark/containers/vector.bench.cpp
+++ test/benchmark/containers/vector.bench.cpp
@@ -4,18 +4,7 @@
 #include <set>
 
 BENCHMARK_TEMPLATE(container_range_constructor, std::vector<int>, ConstantGenerator<int, 42>)->Arg(8<<10);
-BENCHMARK_TEMPLATE(container_range_constructor, std::vector<int>, ConstantStrideGenerator<int, 0, 8>)->Range(64, 8<<10);
-BENCHMARK_TEMPLATE(container_range_constructor, std::vector<int>, ConstantStrideGenerator<int, 0, 512>)->Range(64, 8<<10);
-BENCHMARK_TEMPLATE(container_range_constructor, std::vector<int>, StrideGenerator<int>)->Range(64, 8<<10);
-
-BENCHMARK_TEMPLATE(container_copy_constructor, std::vector<int>, StrideGenerator<int>)->Range(64, 8<<10);
-BENCHMARK_TEMPLATE(container_copy_constructor, std::vector<int>, ConstantStrideGenerator<int, 0, 8>)->Range(8, 8<<10);
-BENCHMARK_TEMPLATE(container_copy_constructor, std::vector<int>, ConstantStrideGenerator<int, 0, 512>)->Range(64, 8<<10);
-BENCHMARK_TEMPLATE(container_copy_constructor, std::vector<int>, RandomGenerator<int>)->Range(64, 8<<10);
-
-BENCHMARK_TEMPLATE(container_copy_assignment, std::vector<int>, ConstantStrideGenerator<int, 0, 8>)->Range(8, 8<<10);
-BENCHMARK_TEMPLATE(container_copy_assignment, std::vector<int>, ConstantStrideGenerator<int, 0, 512>)->Range(64, 8<<10);
-BENCHMARK_TEMPLATE(container_copy_assignment, std::vector<int>, RandomGenerator<int>)->Range(64, 8<<10);
-
+BENCHMARK_TEMPLATE(container_copy_constructor, std::vector<int>, ConstantGenerator<int, 42>)->Arg(8<<10);
+BENCHMARK_TEMPLATE(container_copy_assignment, std::vector<int>, ConstantGenerator<int, 42>)->Arg(8<<10);
 
 BENCHMARK_MAIN()
