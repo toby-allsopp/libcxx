344aa608bc64015a25c1afa04d6c60c9523775e7
Eric Fiselier <eric@efcs.ca>
2015-03-17 11:52:45 -0400

    address review comments

--- test/libcxx/experimental/utilities/tuple/version.pass.cpp
+++ test/libcxx/experimental/utilities/tuple/version.pass.cpp
@@ -15,12 +15,6 @@
 #error _LIBCPP_VERSION not defined
 #endif
 
-#if _LIBCPP_STD_VER > 11
-# ifndef __cpp_lib_experimental_apply
-#   error "__cpp_lib_experimental_apply must be defined"
-# endif
-#endif
-
 int main()
 {
 }
