ab26024782aa7b904da2c7e65fa76ee6fe0884bd
Eric Fiselier <eric@efcs.ca>
2015-03-18 22:56:20 -0400

    add tests

--- test/benchmark/algorithm/find.bench.cpp
+++ test/benchmark/algorithm/find.bench.cpp
@@ -18,4 +18,35 @@ void BM_find(benchmark::State& state) {
 }
 BENCHMARK_TEMPLATE(BM_find, StrideGenerator<int>)->Arg(10000);
 
+template <class Generator>
+void BM_find_if(benchmark::State& state) {
+    auto arr = generate_test_array<Generator>(state.range_x());
+    typedef typename Generator::value_type ValueType;
+    auto val = arr[arr.size() - 1];
+    auto beg = arr.begin();
+    auto end = arr.end();
+    auto pred = [=](ValueType const& lhs) { return lhs == val; };
+    while (state.KeepRunning()) {
+        auto volatile result = std::find_if(beg, end, pred);
+        ((void)result);
+    }
+}
+BENCHMARK_TEMPLATE(BM_find_if, StrideGenerator<int>)->Arg(10000);
+
+
+template <class Generator>
+void BM_find_if_not(benchmark::State& state) {
+    auto arr = generate_test_array<Generator>(state.range_x());
+    typedef typename Generator::value_type ValueType;
+    auto val = arr[arr.size() - 1];
+    auto beg = arr.begin();
+    auto end = arr.end();
+    auto pred = [=](ValueType const& lhs) { return lhs != val; };
+    while (state.KeepRunning()) {
+        auto volatile result = std::find_if_not(beg, end, pred);
+        ((void)result);
+    }
+}
+BENCHMARK_TEMPLATE(BM_find_if_not, StrideGenerator<int>)->Arg(10000);
+
 BENCHMARK_MAIN()
