40a61cf1f9996b4a161392c9d571dc7555620b73
Eric Fiselier <eric@efcs.ca>
2015-04-07 05:03:59 -0400

    stuff

--- test/benchmark/algorithm/alg.nonmodifying/alg.search/search_pred.bench.cpp
+++ test/benchmark/algorithm/alg.nonmodifying/alg.search/search_pred.bench.cpp
@@ -17,17 +17,28 @@ void BM_search_pred(benchmark::State& st) {
     for (int i=st.range_x() - (st.range_y() + 1); i < st.range_x(); ++i) {
         test_arr[i] = test_arr1[i];
     }
-    auto test_arr2 = make_test_array<int>(1, -1);
     while (st.KeepRunning()) {
         DoNotOptimize(std::search(test_arr.begin(), test_arr.end(), test_arr1.begin(), test_arr1.end(), &is_equal));
-        DoNotOptimize(std::search(test_arr.begin(), test_arr.end(), test_arr2.begin(), test_arr2.end(), &is_equal));
         DoNotOptimize(test_arr);
         DoNotOptimize(test_arr1);
-        DoNotOptimize(test_arr2);
     }
 }
 BENCHMARK(BM_search_pred)->ArgPair(1<<14, 1<<4);
 BENCHMARK(BM_search_pred)->ArgPair(1<<14, 1<<14);
 
+
+void BM_search_pred_single_length_pattern(benchmark::State& st) {
+    StrideGenerator<int> g(0, st.range_y());
+    auto test_arr = generate_test_array<int>(st.range_x(), g);
+    auto test_arr1 = make_test_array<int>(1, -1);
+    while (st.KeepRunning()) {
+        DoNotOptimize(std::search(test_arr.begin(), test_arr.end(),
+                                  test_arr1.begin(), test_arr1.end(),
+                                  &is_equal));
+        DoNotOptimize(test_arr);
+        DoNotOptimize(test_arr1);
+    }
+}
+BENCHMARK(BM_search_pred_single_length_pattern)->ArgPair(1<<14);
 
 BENCHMARK_MAIN()
