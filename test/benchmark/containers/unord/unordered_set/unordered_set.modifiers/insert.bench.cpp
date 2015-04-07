b77ca2f64a3b26b90fc811da0a8e648d967453d6
Eric Fiselier <eric@efcs.ca>
2015-04-07 17:16:03 -0400

    stuff

--- test/benchmark/containers/unord/unordered_set/unordered_set.modifiers/insert.bench.cpp
+++ test/benchmark/containers/unord/unordered_set/unordered_set.modifiers/insert.bench.cpp
@@ -9,6 +9,7 @@ using Stride = StrideGenerator<int>;
 
 BENCHMARK_TEMPLATE(container_insert_range, IntSet, Stride, Stride)->ArgPair(1<<14, 1<<8);
 BENCHMARK_TEMPLATE(container_insert_value, IntSet, Stride, Stride)->ArgPair(1<<14, 1<<14);
+BENCHMARK_TEMPLATE(container_insert_value, IntSet, Stride, ConstantGenerator<int, 42>)->ArgPair(1, 1<<14);
 
 BENCHMARK_MAIN()
 
