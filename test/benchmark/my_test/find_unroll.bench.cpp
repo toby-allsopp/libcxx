033dd9ac84a4559c21705ab5e48529d466e95c19
Eric Fiselier <eric@efcs.ca>
2015-04-07 15:13:37 -0400

    stuff

--- test/benchmark/my_test/find_unroll.bench.cpp
+++ test/benchmark/my_test/find_unroll.bench.cpp
@@ -19,187 +19,187 @@ Iter find0(Iter first, Iter last, Tp const& value) {
 template <class Iter, class Tp>
 Iter find2(Iter first, Iter last, Tp const& value) {
     std::ptrdiff_t count = last - first;
-    if (count == 0) return last;
+    if (count == 0) return first;
     std::size_t n = (count + 1) / 2;
     switch (count % 2) {
     case 0:
         do {
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 1:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
         } while(--n > 0);
     }
-    return last;
+    return first;
 }
 
 template <class Iter, class Tp>
 Iter find4(Iter first, Iter last, Tp const& value) {
     std::ptrdiff_t count = last - first;
-    if (count == 0) return last;
+    if (count == 0) return first;
     std::size_t n = (count + 3) / 4;
     switch (count % 4) {
     case 0:
         do {
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 3:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 2:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 1:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
         } while(--n > 0);
     }
-    return last;
+    return first;
 }
 
 template <class Iter, class Tp>
 Iter find8(Iter first, Iter last, Tp const& value) {
     std::ptrdiff_t count = last - first;
-    if (count == 0) return last;
+    if (count == 0) return first;
     std::size_t n = (count + 7) / 8;
     switch (count % 8) {
     case 0:
         do {
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 7:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 6:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 5:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 4:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 3:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 2:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 1:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
         } while(--n > 0);
     }
-    return last;
+    return first;
 }
 
 template <class Iter, class Tp>
 Iter find12(Iter first, Iter last, Tp const& value) {
     std::ptrdiff_t count = last - first;
-    if (count == 0) return last;
+    if (count == 0) return first;
     std::size_t n = (count + 11) / 12;
     switch (count % 12) {
     case 0:
         do {
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 11:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 10:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 9:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 8:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 7:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 6:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 5:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 4:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 3:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 2:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 1:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
         } while(--n > 0);
     }
-    return last;
+    return first;
 }
 
 template <class Iter, class Tp>
 Iter find16(Iter first, Iter last, Tp const& value) {
     std::ptrdiff_t count = last - first;
-    if (count == 0) return last;
+    if (count == 0) return first;
     std::size_t n = (count + 15) / 16;
     switch (count % 16) {
     case 0:
         do {
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 15:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 14:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 13:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 12:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 11:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 10:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 9:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 8:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 7:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 6:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 5:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 4:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 3:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 2:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
             case 1:
-                if (*first == value) return first;
+                if (*first == value) break;
                 ++first;
         } while(--n > 0);
     }
-    return last;
+    return first;
 }
 
 
