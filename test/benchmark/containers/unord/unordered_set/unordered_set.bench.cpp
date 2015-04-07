a0aff903d13bd5bf4a91847d9ede5ddce6006fe8
Eric Fiselier <eric@efcs.ca>
2015-04-07 01:34:58 -0400

    add

--- test/benchmark/containers/unord/unordered_set/unordered_set.bench.cpp
+++ test/benchmark/containers/unord/unordered_set/unordered_set.bench.cpp
@@ -3,22 +3,6 @@
 
 #include <unordered_set>
 
-BENCHMARK_TEMPLATE(container_range_constructor, std::unordered_set<int>, ConstantGenerator<int, 42>)->Arg(8<<12);
-BENCHMARK_TEMPLATE(container_range_constructor, std::unordered_set<int>, StrideGenerator<int>)->Arg(8<<12);
-
-BENCHMARK_TEMPLATE(container_range_constructor, std::unordered_set<int>, ConstantStrideGenerator<int, 0, 512>)->Arg(64);
-BENCHMARK_TEMPLATE(container_range_constructor, std::unordered_set<int>, ConstantStrideGenerator<int, 0, 512>)->Arg(512);
-BENCHMARK_TEMPLATE(container_range_constructor, std::unordered_set<int>, ConstantStrideGenerator<int, 0, 512>)->Arg(8<<12);
-
-BENCHMARK_TEMPLATE(container_copy_constructor, std::unordered_set<int>, ConstantGenerator<int, 42>)->Arg(8<<12);
-BENCHMARK_TEMPLATE(container_copy_constructor, std::unordered_set<int>, StrideGenerator<int>)->Arg(8<<12);
-BENCHMARK_TEMPLATE(container_copy_constructor, std::unordered_set<int>, ConstantStrideGenerator<int, 0, 512>)->Arg(8<<12);
-
-BENCHMARK_TEMPLATE(container_copy_assignment, std::unordered_set<int>, ConstantGenerator<int, 42>)->Arg(8<<12);
-BENCHMARK_TEMPLATE(container_copy_assignment, std::unordered_set<int>, StrideGenerator<int>)->Arg(8<<12);
-BENCHMARK_TEMPLATE(container_copy_assignment, std::unordered_set<int>, ConstantStrideGenerator<int, 0, 512>)->Arg(8<<12);
-
-
 BENCHMARK_TEMPLATE(container_iterate, std::unordered_set<int>, StrideGenerator<int>)->Arg(8<<12);
 
 BENCHMARK_MAIN()
