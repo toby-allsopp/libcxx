7a4ec13942ecf4b2f64075e734bac4e974d51454
Eric Fiselier <eric@efcs.ca>
2016-02-07 06:19:02 -0700

    revert changes to existing tests

--- test/std/containers/unord/unord.map/unord.map.modifiers/insert_const_lvalue.pass.cpp
+++ test/std/containers/unord/unord.map/unord.map.modifiers/insert_const_lvalue.pass.cpp
@@ -19,9 +19,6 @@
 #include <cassert>
 
 #include "min_allocator.h"
-#include "test_macros.h"
-#include "test_types.h"
-#include "count_new.hpp"
 
 int main()
 {
@@ -54,7 +51,7 @@ int main()
         assert(r.first->first == 5.5);
         assert(r.first->second == 4);
     }
-#if TEST_STD_VER >= 11
+#if __cplusplus >= 201103L
     {
         typedef std::unordered_map<double, int, std::hash<double>, std::equal_to<double>,
                             min_allocator<std::pair<const double, int>>> C;
@@ -86,55 +83,4 @@ int main()
         assert(r.first->second == 4);
     }
 #endif
-    {
-        typedef CountingType<0> Key;
-        typedef CountingType<1> Value;
-        typedef std::pair<const Key, Value> ValueTp;
-        typedef std::unordered_map<Key, Value> C;
-        typedef std::pair<C::iterator, bool> R;
-        C c;
-        const ValueTp p; // const lvalue
-        p.first.allow_copies = 1;
-        p.second.allow_copies = 1;
-        R r = c.insert(p);
-        assert(r.second);
-        {
-            DisableAllocationGuard g;
-            r = c.insert(p);
-            assert(!r.second);
-        }
-        ValueTp p2; // non-const lvalue
-        p2.first.data = 42;
-        p2.first.allow_copies = 1;
-        p2.second.allow_copies = 1;
-        r = c.insert(p2);
-        assert(r.second);
-        {
-            DisableAllocationGuard g;
-            r = c.insert(p2);
-            assert(!r.second);
-        }
-    }
-    {
-        typedef int Key;
-        typedef int Value;
-        typedef std::pair<const Key, Value> ValueTp;
-        typedef test_construct_allocator<ValueTp, ValueTp> Alloc;
-        typedef std::unordered_map<Key, Value, std::hash<Key>, std::equal_to<Key>, Alloc> C;
-        typedef std::pair<C::iterator, bool> R;
-        ConstructController cc;
-        Alloc a(&cc);
-        C c(a);
-        const ValueTp p(42, 43); // const lvalue
-        cc.expect(makeArgumentID<const ValueTp &>());
-        R r = c.insert(p);
-        cc.disable();
-        assert(r.second);
-        {
-            DisableAllocationGuard g;
-            ValueTp p1(42, 43);
-            r = c.insert(p1);
-            assert(!r.second);
-        }
-    }
 }
--- test/std/containers/unord/unord.map/unord.map.modifiers/insert_rvalue.pass.cpp
+++ test/std/containers/unord/unord.map/unord.map.modifiers/insert_rvalue.pass.cpp
@@ -22,9 +22,6 @@
 
 #include "MoveOnly.h"
 #include "min_allocator.h"
-#include "test_macros.h"
-#include "test_types.h"
-#include "count_new.hpp"
 
 int main()
 {
@@ -57,7 +54,7 @@ int main()
         assert(r.first->first == 5.5);
         assert(r.first->second == 4);
     }
-#if TEST_STD_VER >= 11
+#ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
     {
         typedef std::unordered_map<MoveOnly, MoveOnly> C;
         typedef std::pair<C::iterator, bool> R;
@@ -87,6 +84,8 @@ int main()
         assert(r.first->first == 5);
         assert(r.first->second == 4);
     }
+#endif  // _LIBCPP_HAS_NO_RVALUE_REFERENCES
+#if __cplusplus >= 201103L
     {
         typedef std::unordered_map<double, int, std::hash<double>, std::equal_to<double>,
                             min_allocator<std::pair<const double, int>>> C;
@@ -117,6 +116,7 @@ int main()
         assert(r.first->first == 5.5);
         assert(r.first->second == 4);
     }
+#ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
     {
         typedef std::unordered_map<MoveOnly, MoveOnly, std::hash<MoveOnly>, std::equal_to<MoveOnly>,
                             min_allocator<std::pair<const MoveOnly, MoveOnly>>> C;
@@ -147,39 +147,6 @@ int main()
         assert(r.first->first == 5);
         assert(r.first->second == 4);
     }
-    {
-        typedef CountingType<0> Key;
-        typedef CountingType<1> Value;
-        typedef std::pair<const Key, Value> ValueTp;
-        typedef std::unordered_map<Key, Value> C;
-        typedef std::pair<C::iterator, bool> R;
-        C c;
-        ValueTp p; // non-const rvalue
-        p.first.allow_copies = 1;
-        p.second.allow_moves = 1;
-        R r = c.insert(std::move(p));
-        assert(r.second);
-        {
-            DisableAllocationGuard g;
-            p.first.reset();
-            p.first.allow_copies = 1;
-            p.second.reset();
-            p.second.data = 0;
-            p.second.allow_moves = 1;
-            r = c.insert(std::move(p));
-            assert(!r.second);
-        }
-        const ValueTp p2; // non-const lvalue
-        p2.first.data = 42;
-        p2.first.allow_copies = 1;
-        p2.second.allow_copies = 1;
-        r = c.insert(std::move(p2));
-        assert(r.second);
-        {
-            DisableAllocationGuard g;
-            r = c.insert(std::move(p2));
-            assert(!r.second);
-        }
-    }
+#endif  // _LIBCPP_HAS_NO_RVALUE_REFERENCES
 #endif
 }
