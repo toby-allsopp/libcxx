73f0a4f86c1d8643ac425440eee9c37d67628b58
Eric Fiselier <eric@efcs.ca>
2016-05-06 19:02:26 -0600

    changes

--- test/std/experimental/filesystem/class.directory_entry/directory_entry.obs/symlink_status.pass.cpp
+++ test/std/experimental/filesystem/class.directory_entry/directory_entry.obs/symlink_status.pass.cpp
@@ -13,8 +13,8 @@
 
 // class directory_entry
 
-// file_status status() const;
-// file_status status(error_code const&) const noexcept;
+// file_status symlink_status() const;
+// file_status symlink_status(error_code&) const noexcept;
 
 #include <experimental/filesystem>
 #include <type_traits>
@@ -22,22 +22,21 @@
 
 #include "filesystem_test_helper.hpp"
 
-int main()
-{
+int main() {
   using namespace fs;
   {
     const directory_entry e("foo");
     std::error_code ec;
-    static_assert(std::is_same<decltype(e.status()), file_status>::value, "");
-    static_assert(std::is_same<decltype(e.status(ec)), file_status>::value, "");
-    static_assert(noexcept(e.status()) == false, "");
-    static_assert(noexcept(e.status(ec)) == true, "");
+    static_assert(std::is_same<decltype(e.symlink_status()), file_status>::value, "");
+    static_assert(std::is_same<decltype(e.symlink_status(ec)), file_status>::value, "");
+    static_assert(noexcept(e.symlink_status()) == false, "");
+    static_assert(noexcept(e.symlink_status(ec)) == true, "");
   }
   auto TestFn = [](path const& p) {
     const directory_entry e(p);
     std::error_code pec, eec;
-    file_status ps = fs::status(p, pec);
-    file_status es = e.status(eec);
+    file_status ps = fs::symlink_status(p, pec);
+    file_status es = e.symlink_status(eec);
     assert(ps.type() == es.type());
     assert(ps.permissions() == es.permissions());
     assert(pec == eec);
