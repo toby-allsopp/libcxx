1a84c96843e6c516405da9bb33669dd51351bee5
Eric Fiselier <eric@efcs.ca>
2016-05-07 17:24:47 -0600

    finish iterate tests

--- src/experimental/directory_iterator.cpp
+++ src/experimental/directory_iterator.cpp
@@ -12,14 +12,28 @@ inline error_code capture_errno() {
     return error_code{errno, std::generic_category()};
 }
 
-inline bool set_error_or_throw(std::error_code& my_ec, std::error_code* user_ec,
-                               const char* msg, const path& p)
+template <class ...Args>
+inline bool capture_error_or_throw(std::error_code* user_ec,
+                                   const char* msg, Args&&... args)
+{
+    std::error_code my_ec = capture_errno();
+    if (user_ec) {
+        *user_ec = my_ec;
+        return true;
+    }
+    throw filesystem_error(msg, std::forward<Args>(args)..., my_ec);
+}
+
+template <class ...Args>
+inline bool set_error_or_throw(std::error_code& my_ec,
+                               std::error_code* user_ec,
+                               const char* msg, Args&&... args)
 {
     if (user_ec) {
         *user_ec = my_ec;
         return true;
     }
-    throw filesystem_error(msg, p, my_ec);
+    throw filesystem_error(msg, std::forward<Args>(args)..., my_ec);
 }
 
 typedef path::string_type string_type;
@@ -32,146 +46,111 @@ inline DIR *posix_opendir(const string_type& p, error_code& ec) {
     return ret;
 }
 
-inline string_type posix_readdir_r(DIR *dir_stream, error_code *ec) {
+inline string_type posix_readdir_r(DIR *dir_stream, error_code& ec) {
     struct dirent dir_entry;
     struct dirent *dir_entry_ptr{nullptr};
     int ret;
     if ((ret = ::readdir_r(dir_stream,  &dir_entry,  &dir_entry_ptr)) != 0)
     {
-        error_code m_ec = capture_errno();
-        if (ec) {
-            *ec = m_ec;
-            return {};
-        } else {
-            throw filesystem_error("fs::posix_readdir_r", m_ec);
-        }
+        ec = capture_errno();
+        return {};
     }
-    if (ec) ec->clear();
-    if (dir_entry_ptr == nullptr) return string_type{};
-    return string_type{dir_entry.d_name};
+    ec.clear();
+    return dir_entry_ptr ? dir_entry.d_name : string_type{};
 }
 
-inline void posix_closedir(DIR *dir_stream,  error_code *ec) {
+inline std::error_code posix_closedir(DIR *dir_stream) {
     if (::closedir(dir_stream) == -1) {
-        error_code m_ec = capture_errno();
-        if (ec) {
-            *ec = m_ec;
-            return;
-        } else {
-            throw filesystem_error("fs::posix_closedir", m_ec);
-        }
+        return capture_errno();
     }
-    if (ec) ec->clear();
+    return {};
 }
 
 }}                                                       // namespace detail
 
 using detail::set_error_or_throw;
+using detail::capture_error_or_throw;
 
 class __dir_stream {
 public:
-    __dir_stream() noexcept = default;
-    __dir_stream(const path& root, error_code& ec);
-
-    __dir_stream(__dir_stream&&) noexcept;
-    __dir_stream& operator=(__dir_stream&&) noexcept;
-
-    // NO COPYING
+    __dir_stream() = delete;
     __dir_stream(const __dir_stream&) = delete;
     __dir_stream& operator=(const __dir_stream&) = delete;
 
-    ~__dir_stream() noexcept;
+    __dir_stream(const path& root, error_code& ec)
+        : __stream_(detail::posix_opendir(root.native(),  ec)),
+          __root_(root)
+    {}
 
-    bool good() const noexcept
-    { return __stream_ != nullptr; }
+    ~__dir_stream() noexcept
+      { if (__stream_) close(); }
 
-    path advance(error_code *ec=nullptr);
+    bool good() const noexcept { return __stream_ != nullptr; }
 
-    void close(error_code *ec=nullptr);
-
-private:
-    DIR *__stream_{nullptr};
-};
-
-__dir_stream::__dir_stream(const path& p, error_code & ec) {
-    __stream_ = detail::posix_opendir(p.native(),  ec);
-}
-
-__dir_stream::~__dir_stream() noexcept { 
-    if (__stream_) {
-        // we don't want to throw,  so eat the error if one exists
-        error_code ec;
-        this->close(&ec); 
+    path advance(error_code &ec) {
+        while (true) {
+            auto str = detail::posix_readdir_r(__stream_,  ec);
+            if (str == "." || str == "..")
+                continue;
+            if (ec || str.empty()) {
+                close();
+                return str;
+            } else {
+                return __root_ / str;
+            }
+        }
     }
-}
+private:
+    std::error_code close() noexcept {
+        std::error_code ec = detail::posix_closedir(__stream_);
+        __stream_ = nullptr;
+        return ec;
+    }
 
-path __dir_stream::advance(error_code *ec) {
-    path p{};
-    if (__stream_ == nullptr) return path{};
-    error_code m_ec;
-    auto str = detail::posix_readdir_r(__stream_,  &m_ec);
-    if (m_ec || str.empty()) close();
-    if (ec && m_ec) *ec = m_ec;
-    return path{str};
-}
-
-void __dir_stream::close(error_code *ec) {
-    if (__stream_ == nullptr) return;
-    // incase posix_closedir throws, set __stream_
-    // to nullptr before the call
-    DIR *tmp = __stream_;
-    __stream_ = nullptr;
-    detail::posix_closedir(tmp, ec);
-}
+    DIR * __stream_{nullptr};
+public:
+    path __root_;
+};
 
 // directory_iterator
 
 directory_iterator::directory_iterator(const path& p, error_code *ec,
                                        directory_options opts)
-  : __options_(opts)
 {
     std::error_code m_ec;
-    __stream_ = std::make_shared<__dir_stream>(p, m_ec);
+    __imp_ = std::make_shared<__dir_stream>(p, m_ec);
     if (m_ec) {
-        __make_end();
+        __imp_.reset();
 
         const bool allow_eacess =
-            bool(__options_ & directory_options::skip_permission_denied);
+            bool(opts & directory_options::skip_permission_denied);
         const std::error_code eaccess_cond =
             std::make_error_code(std::errc::permission_denied);
 
         if (allow_eacess && m_ec == eaccess_cond)
             return;
 
-        if (set_error_or_throw(m_ec, ec, "directory_iterator could not be "
-                                         " constructed from path", p))
-            return;
+        set_error_or_throw(m_ec, ec, "directory_iterator could not be "
+                                        " constructed from path", p);
+        return;
     }
-    __root_path_ = p;
     __increment(ec);
 }
 
 directory_iterator& directory_iterator::__increment(error_code *ec)
 {
-    if (!__stream_) return *this;
-    path part;
-    while (__stream_->good()) {
-        part = __stream_->advance(ec);
-        if (part != "." && part != "..")
-            break;
+    _LIBCPP_ASSERT(__stream_, "Attempting to increment an invalid iterator");
+    std::error_code m_ec;
+    __elem_.assign(__imp_->advance(m_ec));
+    if (!__imp_->good()) {
+        __imp_.reset();
+        if (m_ec)
+            set_error_or_throw(m_ec, ec, "directory_iterator::operator++()");
+    } else {
+        if (ec) ec->clear();
     }
-
-    if (!__stream_->good())
-        __make_end();
-    else
-        __elem_.assign(__root_path_ / part);
     return *this;
-}
 
-void directory_iterator::__make_end() {
-    __stream_.reset();
-    __root_path_.clear();
-    __elem_.assign(path{""});
 }
 
 // recursive_directory_iterator
