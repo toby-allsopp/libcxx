084a60f8c297e4136e77363b4faace39bbbfbb48
Eric Fiselier <eric@efcs.ca>
2016-05-29 15:02:23 -0600

    system_complete test

--- test/std/experimental/filesystem/fs.op.funcs/fs.op.system_complete/system_complete.pass.cpp
+++ test/std/experimental/filesystem/fs.op.funcs/fs.op.system_complete/system_complete.pass.cpp
@@ -11,7 +11,11 @@
 
 // <experimental/filesystem>
 
-// path absolute(const path& p, const path& base=current_path());
+// path system_complete(const path& p);
+// path system_complete(const path& p, error_code& ec);
+
+// Note: For POSIX based operating systems, 'system_complete(p)' has the
+// same semantics as 'absolute(p, current_path())'.
 
 #include <experimental/filesystem>
 #include <type_traits>
@@ -23,81 +27,18 @@
 
 using namespace std::experimental::filesystem;
 
-TEST_SUITE(filesystem_absolute_path_test_suite)
+TEST_SUITE(filesystem_system_complete_test_suite)
 
 TEST_CASE(absolute_signature_test)
 {
     const path p; ((void)p);
-    ASSERT_NOT_NOEXCEPT(absolute(p));
-    ASSERT_NOT_NOEXCEPT(absolute(p, p));
+    std::error_code ec; ((void)ec);
+    ASSERT_NOT_NOEXCEPT(system_complete(p));
+    ASSERT_NOT_NOEXCEPT(system_complete(p, ec));
 }
 
-// There are 4 cases is the proposal for absolute path.
-// Each scope tests one of the cases.
-TEST_CASE(absolute_path_test)
-{
-    // has_root_name() && has_root_directory()
-    {
-        const path p("//net/foo");
-        const path base("//net/bar/baz");
-        TEST_REQUIRE(p.has_root_name());
-        TEST_REQUIRE(p.has_root_directory());
-        TEST_CHECK(p.is_absolute());
-        path ret = absolute(p, base);
-        TEST_CHECK(ret.is_absolute());
-        TEST_CHECK(ret == p);
-    }
-    // !has_root_name() && has_root_directory()
-    {
-        const path p("/foo");
-        const path base("//net/bar");
-        TEST_REQUIRE(not p.has_root_name());
-        TEST_REQUIRE(p.has_root_directory());
-        TEST_CHECK(p.is_absolute());
-        // ensure absolute(base) is not recursivly called
-        TEST_REQUIRE(base.has_root_name());
-        TEST_REQUIRE(base.has_root_directory());
 
-        path ret = absolute(p, base);
-        TEST_CHECK(ret.is_absolute());
-        TEST_CHECK(ret.has_root_name());
-        TEST_CHECK(ret.root_name() == path("//net"));
-        TEST_CHECK(ret.has_root_directory());
-        TEST_CHECK(ret.root_directory() == path("/"));
-        TEST_CHECK(ret == path("//net/foo"));
-    }
-    // has_root_name() && !has_root_directory()
-    {
-        const path p("//net");
-        const path base("//net/foo/bar");
-        TEST_REQUIRE(p.has_root_name());
-        TEST_REQUIRE(not p.has_root_directory());
-        TEST_CHECK(not p.is_absolute());
-        // absolute is called recursivly on base. The following conditions
-        // must be true for it to return base unmodified
-        TEST_REQUIRE(base.has_root_name());
-        TEST_REQUIRE(base.has_root_directory());
-        path ret = absolute(p, base);
-        const path expect("//net/foo/bar");
-        TEST_CHECK(ret.is_absolute());
-        TEST_CHECK(ret == path("//net/foo/bar"));
-    }
-    // !has_root_name() && !has_root_directory()
-    {
-        const path p("bar/baz");
-        const path base("//net/foo");
-        TEST_REQUIRE(not p.has_root_name());
-        TEST_REQUIRE(not p.has_root_directory());
-        TEST_REQUIRE(base.has_root_name());
-        TEST_REQUIRE(base.has_root_directory());
-
-        path ret = absolute(p, base);
-        TEST_CHECK(ret.is_absolute());
-        TEST_CHECK(ret == path("//net/foo/bar/baz"));
-    }
-}
-
-TEST_CASE(absolute_path_with_default_base)
+TEST_CASE(basic_system_complete_tests)
 {
     const path testCases[] = {
         "//net/foo", //  has_root_name() &&  has_root_directory()
@@ -107,7 +48,7 @@ TEST_CASE(absolute_path_with_default_base)
     };
     const path base = current_path();
     for (auto& p : testCases) {
-        const path ret = absolute(p);
+        const path ret = system_complete(p);
         const path expect = absolute(p, base);
         TEST_CHECK(ret.is_absolute());
         TEST_CHECK(ret == expect);
