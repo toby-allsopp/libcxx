647a6d965901e54f0864d3484c15f11433bfbdce
Eric Fiselier <eric@efcs.ca>
2016-05-29 14:45:31 -0600

    test

--- test/std/experimental/filesystem/fs.op.funcs/fs.op.symlink_status/symlink_status.pass.cpp
+++ test/std/experimental/filesystem/fs.op.funcs/fs.op.symlink_status/symlink_status.pass.cpp
@@ -16,6 +16,9 @@
 
 #include <experimental/filesystem>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "test_macros.h"
 #include "rapid-cxx-test.hpp"
@@ -106,7 +109,14 @@ TEST_CASE(test_symlink_status_cannot_resolve)
     }
 }
 
-TEST_CASE(status_file_types_test)
+TEST_CASE(status_create_socket_file)
+{
+    scoped_test_env env;
+    const path p = env.make_env_path("socket");
+    TEST_REQUIRE(::mknod(p.c_str(), 0600 | S_IFSOCK, 0) != -1);
+}
+
+TEST_CASE(symlink_status_file_types_test)
 {
     scoped_test_env env;
     struct TestCase {
