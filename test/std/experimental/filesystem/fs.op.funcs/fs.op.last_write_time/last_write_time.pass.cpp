82cb1641f7056471d1ac288fc530897e1d3e9a90
Eric Fiselier <eric@efcs.ca>
2016-06-18 19:31:59 -0600

    stuff

--- test/std/experimental/filesystem/fs.op.funcs/fs.op.last_write_time/last_write_time.pass.cpp
+++ test/std/experimental/filesystem/fs.op.funcs/fs.op.last_write_time/last_write_time.pass.cpp
@@ -209,6 +209,7 @@ TEST_CASE(set_last_write_time_dynamic_env_test)
 
         TEST_CHECK(got_time != old_time);
         if (TC.new_time < epoch_time) {
+            std::cout << "On file: " << p << " " << got_time.time_since_epoch().count() << std::endl;
             TEST_CHECK(got_time <= TC.new_time);
             TEST_CHECK(got_time > TC.new_time - Sec(1));
         } else {
