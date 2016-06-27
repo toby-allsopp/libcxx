9f6ad1969a12c11a53b1d24e0e612f63b616df26
Eric Fiselier <eric@efcs.ca>
2016-06-26 21:43:47 -0600

    add monostate

---
+++ test/std/variant/variant.monostate/monostate.pass.cpp
@@ -0,0 +1,31 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03, c++11, c++14
+
+// <variant>
+
+// struct monostate {};
+
+
+#include <variant>
+#include <type_traits>
+#include <cassert>
+
+int main()
+{
+    using M = std::monostate;
+    static_assert(std::is_trivially_default_constructible<M>::value, "");
+    static_assert(std::is_trivially_copy_constructible<M>::value, "");
+    static_assert(std::is_trivially_copy_assignable<M>::value, "");
+    static_assert(std::is_trivially_destructible<M>::value, "");
+    constexpr M m{};
+    ((void)m);
+}
