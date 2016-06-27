e8c01f1dee92aca23331a2954a5d40edb431485c
Eric Fiselier <eric@efcs.ca>
2016-06-27 11:44:22 -0600

    add ops

---
+++ test/std/variant/variant.monostate.relops/relops.pass.cpp
@@ -0,0 +1,55 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03, c++11, c++14
+
+// <variant>
+
+// constexpr bool operator<(monostate, monostate) noexcept { return false; }
+// constexpr bool operator>(monostate, monostate) noexcept { return false; }
+// constexpr bool operator<=(monostate, monostate) noexcept { return true; }
+// constexpr bool operator>=(monostate, monostate) noexcept { return true; }
+// constexpr bool operator==(monostate, monostate) noexcept { return true; }
+// constexpr bool operator!=(monostate, monostate) noexcept { return false; }
+
+#include <variant>
+#include <type_traits>
+#include <cassert>
+
+int main()
+{
+    using M = std::monostate;
+    constexpr M m1;
+    constexpr M m2;
+    {
+        static_assert((m1 < m2) == false, "");
+        static_assert(noexcept(m1 < m2), "");
+    }
+    {
+        static_assert((m1 > m2) == false, "");
+        static_assert(noexcept(m1 > m2), "");
+    }
+    {
+        static_assert((m1 <= m2) == true, "");
+        static_assert(noexcept(m1 <= m2), "");
+    }
+    {
+        static_assert((m1 >= m2) == true, "");
+        static_assert(noexcept(m1 >= m2), "");
+    }
+    {
+        static_assert((m1 == m2) == true, "");
+        static_assert(noexcept(m1 == m2), "");
+    }
+    {
+        static_assert((m1 != m2) == false, "");
+        static_assert(noexcept(m1 != m2), "");
+    }
+}
