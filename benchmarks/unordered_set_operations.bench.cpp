09630b3fb3b34661d1ec951e622ec40ec02c9d9b
Eric Fiselier <eric@efcs.ca>
2016-07-11 16:22:17 -0600

    update benchmarks

--- benchmarks/unordered_set_operations.bench.cpp
+++ benchmarks/unordered_set_operations.bench.cpp
@@ -14,11 +14,28 @@ std::vector<IntT> getInputs(size_t N) {
     return inputs;
 }
 
+template <class IntT>
+std::vector<IntT> getInputs2(size_t N) {
+    std::vector<IntT> inputs;
+    for (size_t i=0; i < N; ++i) {
+        inputs.push_back(i + N);
+    }
+    return inputs;
+}
+
 BENCHMARK_CAPTURE(BM_ContainerInsertValue, unordered_set_uint32,
     std::unordered_set<uint32_t>{}, &getInputs<uint32_t>)->Arg(1024);
 
+BENCHMARK_CAPTURE(BM_ContainerInsertValue,
+    unordered_set_uint32_larger_than_bucket_count,
+    std::unordered_set<uint32_t>{}, &getInputs2<uint32_t>)->Arg(1024);
+
 BENCHMARK_CAPTURE(BM_ContainerFind, unordered_set_uint32,
     std::unordered_set<uint32_t>{}, getInputs<uint32_t>)->Arg(1024);
+
+BENCHMARK_CAPTURE(BM_ContainerFind,
+    unordered_set_uint32_larger_than_bucket_count,
+    std::unordered_set<uint32_t>{}, getInputs2<uint32_t>)->Arg(1024);
 
 
 BENCHMARK_MAIN()
