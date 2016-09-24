45c9778edd97153e1752390dbbc9c48b6fc15de4
Eric Fiselier <eric@efcs.ca>
2016-09-24 16:22:32 -0600

    Remove _LIBCPP_HAS_NO_DEFAULTED_FUNCTIONS

--- src/optional.cpp
+++ src/optional.cpp
@@ -11,14 +11,6 @@
 
 _LIBCPP_BEGIN_NAMESPACE_EXPERIMENTAL
 
-#ifdef _LIBCPP_HAS_NO_DEFAULTED_FUNCTIONS
-
-bad_optional_access::~bad_optional_access() _NOEXCEPT {}
-
-#else
-
 bad_optional_access::~bad_optional_access() _NOEXCEPT = default;
-
-#endif
 
 _LIBCPP_END_NAMESPACE_EXPERIMENTAL
