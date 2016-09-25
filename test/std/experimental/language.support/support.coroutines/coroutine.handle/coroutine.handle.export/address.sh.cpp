07f79407804172bfa20fdfbc9955d56d3de2187b
Eric Fiselier <eric@efcs.ca>
2016-09-24 22:28:14 -0600

    more tests

---
+++ test/std/experimental/language.support/support.coroutines/coroutine.handle/coroutine.handle.export/address.sh.cpp
@@ -0,0 +1,50 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03, c++11
+// REQUIRES: fcoroutines
+
+// RUN: %build -fcoroutines
+// RUN: %run
+
+// <experimental/coroutine>
+
+// template <class Promise = void>
+// struct coroutine_handle;
+
+// void* address() const noexcept
+
+#include <experimental/coroutines>
+#include <type_traits>
+#include <cassert>
+
+namespace coro = std::experimental;
+
+template <class C>
+void do_test() {
+  {
+    const C c = {}; ((void)c);
+    static_assert(noexcept(c.address()), "");
+    // FIXME: Should the return type not be 'C'?
+    static_assert(std::is_same<decltype(c.address()), void*>::value, "");
+    assert(c.address() == nullptr);
+  }
+  {
+    int dummy = 42;
+    C c = C::from_address(&dummy);
+    assert(c.address() == &dummy);
+  }
+}
+
+int main()
+{
+  do_test<coro::coroutine_handle<>>();
+  do_test<coro::coroutine_handle<int>>();
+}
