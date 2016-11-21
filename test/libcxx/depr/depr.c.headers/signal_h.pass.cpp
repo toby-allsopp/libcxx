065024ed5db01f4f6423b93a11da24c15fcfbf94
Eric Fiselier <eric@efcs.ca>
2016-11-20 18:23:54 -0700

    add signal tests

---
+++ test/libcxx/depr/depr.c.headers/signal_h.pass.cpp
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// test <signal.h>
+
+#include <signal.h>
+
+#ifndef _LIBCPP_VERSION
+#error _LIBCPP_VERSION should be defined
+#endif
+
+int main()
+{
+}
